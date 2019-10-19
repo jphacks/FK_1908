ebe634318521f89cd1bd4f6cbfe9410b1bc694a1
fumiya-kume <fumiya.kume@hotmail.com>
2019-10-20 03:21:29 +0900

    fix main faragment

--- android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/MainFragment.kt
+++ android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/MainFragment.kt
@@ -1,43 +1,29 @@
 package jp.hacks.smartbread.ui.main
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
 import android.media.MediaPlayer
-import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.camera.core.CameraX
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataType
-import jp.hacks.smartbread.R
-import kotlinx.android.synthetic.main.main_fragment.*
-import java.util.*
-import androidx.annotation.NonNull
+import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
-import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.fitness.Fitness
-import com.google.android.gms.fitness.request.DataReadRequest
-import java.util.concurrent.TimeUnit
-import java.util.Locale
 import jp.hacks.smartbread.R
 import kotlinx.android.synthetic.main.main_fragment.main_fragment_pay_meet_button
 import kotlinx.android.synthetic.main.main_fragment.save_image_button
 import kotlinx.android.synthetic.main.main_fragment.saved_image_finder
 import kotlinx.android.synthetic.main.main_fragment.test_tts_button
 import kotlinx.android.synthetic.main.main_fragment.view_finder
+import java.util.Locale
 
 class MainFragment : Fragment() {
 
