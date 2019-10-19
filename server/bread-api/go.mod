96fe38f4a650e8e354bb19eff15b7d6f392204ce
arabian9ts <nok14.tm59@gmail.com>
2019-10-19 17:32:41 +0900

    service

--- server/bread-api/go.mod
+++ server/bread-api/go.mod
@@ -8,4 +8,5 @@ require (
 	golang.org/x/net v0.0.0-20191014212845-da9a3fd4c582 // indirect
 	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
 	googlemaps.github.io/maps v0.0.0-20191014172202-ce2e58e026c5 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- server/bread-api/main.go
+++ server/bread-api/main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/kr/pretty"
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
-
-	"github.com/jphacks/FK_1908/server/bread-api/config"
+	"gopkg.in/yaml.v2"
 )
 
+type Credentials struct {
+	MapAPIKey string `yaml:"map"`
+}
+
+func newCred() *Credentials {
+	configPath := fmt.Sprintf("config/%s", "credentials.yml")
+
+	buf, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to load env credentials")
+	}
+
+	credentials := &Credentials{}
+	err = yaml.Unmarshal(buf, credentials)
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to map credentials to struct")
+	}
+
+	return credentials
+}
+
 func main() {
-	key := config.Credential().MapAPIKey
-	c, err := maps.NewClient(maps.WithAPIKey(key))
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
-	}
 
-	r := &maps.DistanceMatrixRequest{
-		Origins:      []string{"博多駅"},
-		Destinations: []string{"新飯塚駅"},
-	}
-	
-	resp, err := c.DistanceMatrix(context.Background(), r)
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
-	}
-
-	pretty.Println(resp)
 }
