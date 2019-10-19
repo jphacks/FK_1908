8478d3865dd1e2dc4b9a630a8babf30e5b28f39d
fumiya-kume <fumiya.kume@hotmail.com>
2019-10-19 10:24:01 +0900

    add android app

---
+++ android/Smartbread/app/src/main/java/jp/hacks/smartbread/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.hacks.smartbread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.hacks.smartbread.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.container, MainFragment.newInstance())
+        .commitNow()
+    }
+  }
+}
