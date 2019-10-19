dc5bae1429312e19b4933f769838197344ea0743
arabian9ts <nok14.tm59@gmail.com>
2019-10-19 23:05:29 +0900

    parallelize api calls

--- server/bread-api/alarm/lambda_function.rb
+++ server/bread-api/alarm/lambda_function.rb
@@ -3,20 +3,42 @@ require 'net/http'
 require 'uri'
 require 'date'
 require 'rss'
+require 'openssl'
 
 def lambda_handler(event:, context:)
+  threads = []
+  results = {}
+  mutex = Mutex::new
+
   origin = event['origin']
   dest = event['dest']
-  duration = fetch_duration(origin: origin, dest: dest)
+
+  threads << Thread.start do
+    duration = fetch_duration(origin: origin, dest: dest)
+    mutex.synchronize { results[:duration] = duration }
+  end
+
+  threads << Thread.start do
+    weather = fetch_weather(area: origin)
+    mutex.synchronize { results[:origin_weather] = weather }
+  end
+
+  threads << Thread.start do
+    weather = fetch_weather(area: dest)
+    mutex.synchronize { results[:dest_weather] = weather }
+  end
+
+  threads.each(&:join)
 
   tomorrow = Time.now.localtime.to_date.next_day.to_time
   default_time = DateTime.new(tomorrow.year, tomorrow.month, tomorrow.day, tomorrow.hour, 9, 0, "+09:00")
-  weather = fetch_weather(origin: origin, dest: dest)
-  alarm = default_time.to_time - duration["value"].to_i - prepare_map(weather: weather)
+
+  duration = results[:duration]
+  alarm = default_time.to_time - duration["value"].to_i - prepare_map(weather: results[:origin_weather]) - prepare_map(weather: results[:dest_weather])
 
   { statusCode: 200, data: { alarm: alarm } }
   rescue => e
-  { statusCode: 400, error: e, event: event, duration: duration["value"] }
+  { statusCode: 400, error: e, event: event, results: results }
 end
 
 def credential
@@ -37,7 +59,7 @@ def fetch_duration(origin:, dest:)
   data = JSON.parse(response.body)
 end
 
-def fetch_weather(origin:, dest:)
+def fetch_weather(area:)
   req_url = "https://weather.goo.ne.jp/area/8210.rdf"
   rss = RSS::Parser.parse(req_url, false)
   weather = rss.channel.item.description
