139d43f7bd8c3926ae8885ed75acbd51aa7384b7
hunachi <hanahanahunachi@gmail.com>
2019-10-19 21:30:57 +0900

    Create GoogleFitApiDebug.

--- android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/health/HealthPermissionUseCaseImpl.kt
+++ android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/health/HealthPermissionUseCaseImpl.kt
@@ -1,5 +1,6 @@
 package jp.hacks.smartbread.ui.main.health
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.FitnessOptions
