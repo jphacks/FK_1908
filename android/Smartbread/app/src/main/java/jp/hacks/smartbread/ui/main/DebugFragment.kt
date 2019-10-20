7cdd645569fa6cb0c576cfed43a29f0d22603cdd
fumiya-kume <fumiya.kume@hotmail.com>
2019-10-20 10:08:37 +0900

    add navigate face fragment on budeg fragment

--- android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/DebugFragment.kt
+++ android/Smartbread/app/src/main/java/jp/hacks/smartbread/ui/main/DebugFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import jp.hacks.smartbread.MainActivity
 import jp.hacks.smartbread.databinding.FragmentDebugBinding
 import jp.hacks.smartbread.ui.main.tts.TTSServiceImpl
 import jp.hacks.smartbread.ui.main.wake.StartBurnBreadUsecaseImpl
 import jp.hacks.smartbread.ui.main.wake.StopBurnBreadUsecaseImpl
+import kotlinx.android.synthetic.main.fragment_debug.view.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -50,6 +52,11 @@ internal class DebugFragment : Fragment() {
             GlobalScope.launch {
                 ttsService.speach("Hello World")
             }
+        }
+
+        binding.fragmentDebugNavigateFaceFragmentButton.setOnClickListener {
+            val mainActivity = activity as MainActivity
+            mainActivity.navigateToFaceFragment()
         }
 
         return binding.root
